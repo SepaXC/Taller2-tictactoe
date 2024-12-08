f403675909b80abc226ceb0273eab7adf7ac15b5
vicente3000 <118288508+vicente3000@users.noreply.github.com>
2024-12-08 22:04:04 +0000

    cambios

---
+++ build/CMakeFiles/Taller2-TicTacToe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Taller2-TicTacToe.dir/Nodo.cpp.o"
+  "CMakeFiles/Taller2-TicTacToe.dir/Tablero.cpp.o"
+  "CMakeFiles/Taller2-TicTacToe.dir/TableroAB.cpp.o"
+  "CMakeFiles/Taller2-TicTacToe.dir/main.cpp.o"
+  "Taller2-TicTacToe"
+  "Taller2-TicTacToe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Taller2-TicTacToe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
